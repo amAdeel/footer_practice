774d7894e49ed4b8bea9a8801e86f15b19c2ece5
amAdeel <adeeleducationpurpose@gmail.com>
2024-08-18 18:27:57 +0500

    1st comit

---
+++ constants/index.ts
@@ -0,0 +1,39 @@
+
+import { v4 as uuidv4 } from 'uuid';
+  
+  
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Company',
+      links: [
+        { id: uuidv4(), label: 'Agencies' },
+        { id: uuidv4(), label: 'Blog' },
+        { id: uuidv4(), label: 'Brands' },
+        { id: uuidv4(), label: 'Login' },
+        { id: uuidv4(), label: 'Signup' },
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: [
+        { id: uuidv4(), label: 'Climbing xixixi' },
+        { id: uuidv4(), label: 'Maher' },
+        { id: uuidv4(), label: 'Adeel' },
+      ],
+    },
+  ];
+  
+  
+
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+    ],
+  };
